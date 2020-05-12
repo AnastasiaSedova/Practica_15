5a4a2bf0cc7f590d86100f8dea3120539b20c9a7
AnastasiaSedova <64019919+AnastasiaSedova@users.noreply.github.com>
2020-05-12 15:28:12 +0300

    Практика_15_2

---
+++ Практика_15_2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Практика_15_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.milesTBx = new System.Windows.Forms.TextBox();
+            this.OKBtn = new System.Windows.Forms.Button();
+            this.kmTbx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "мили";
+            // 
+            // milesTBx
+            // 
+            this.milesTBx.Location = new System.Drawing.Point(93, 26);
+            this.milesTBx.Name = "milesTBx";
+            this.milesTBx.Size = new System.Drawing.Size(235, 26);
+            this.milesTBx.TabIndex = 1;
+            // 
+            // OKBtn
+            // 
+            this.OKBtn.Location = new System.Drawing.Point(25, 70);
+            this.OKBtn.Name = "OKBtn";
+            this.OKBtn.Size = new System.Drawing.Size(140, 42);
+            this.OKBtn.TabIndex = 2;
+            this.OKBtn.Text = "Ok";
+            this.OKBtn.UseVisualStyleBackColor = true;
+            this.OKBtn.Click += new System.EventHandler(this.OKBtn_Click);
+            // 
+            // kmTbx
+            // 
+            this.kmTbx.Location = new System.Drawing.Point(25, 133);
+            this.kmTbx.Name = "kmTbx";
+            this.kmTbx.Size = new System.Drawing.Size(303, 26);
+            this.kmTbx.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 212);
+            this.Controls.Add(this.kmTbx);
+            this.Controls.Add(this.OKBtn);
+            this.Controls.Add(this.milesTBx);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Мили-километры";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox milesTBx;
+        private System.Windows.Forms.Button OKBtn;
+        private System.Windows.Forms.TextBox kmTbx;
+    }
+}
+
---
+++ Практика_15_2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_15_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OKBtn_Click(object sender, EventArgs e)
+        {
+            if (milesTBx.Text == "")
+                MessageBox.Show("Выедите значение");
+            else
+                kmTbx.Text = "Указанное число миль равно: "+(double.Parse(milesTBx.Text) * 1.6).ToString()+" километров";
+        }
+    }
+}
